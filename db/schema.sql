922887e8e82a2d3e51173f59d59abf65409232fc
Chris Wallace <Chrisw1096@gmail.com>
2022-08-02 17:36:19 +1000

    seeds file pop

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS school_people;
+CREATE DATABASE school_people;
+
+USE school_people;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,26 @@
-DROP DATABASE IF EXISTS department;
-CREATE DATABASE department;
+INSERT INTO department (id, name)
+VALUES (1001, "IT"),
+(1002,"Maths"),
+(1003,"Science"),
+(1004,"English"),
+(1005,"History");
 
-USE department;
+INSERT INTO role (id, title, salary)
+VALUES (101, "Teacher", 25000),
+(303, "Manager", 600000),
+(404, "Principal", 100000);
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
-);
-CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT REFERENCES employee(id),
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL
-)
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, "Chris", "Wallace", 101),
+(2, "Bob", "Wall", 101),
+(3, "Greg", "Ace", 101),
+(4, "Steeve", "Acer", 101),
+(5, "Jen", "Sanger", 303),
+(6, "Alex", "Jones", 404),
+(7, "John", "Middendorf", 303),
+(8, "Harrold", "Hippo", 101),
+(9, "Arthur", "Miller", 101),
+(10, "Chinua", "Achebe", 303),
+(11, "Margaret", "Atwood", 101),
+(12, "Gabriel", "Garcia Marquez", 404),
+(13, "Simone", "de Beauvoir", 101);
